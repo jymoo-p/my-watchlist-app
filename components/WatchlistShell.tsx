795f7583c5c4cb65a2cc4858c0f205fae3bfdc71
My Watchlist User <user@example.com>
2026-05-02 17:24:20 +0530

    google authentication

--- components/WatchlistShell.tsx
+++ components/WatchlistShell.tsx
@@ -136,6 +136,7 @@ export default function WatchlistShell({ view }: WatchlistShellProps) {
       setWatchList([]);
       setWatchedList([]);
       setWeeklyPicks([]);
+      setHasLoadedRemote(false);
     }
   }, [user, authLoading]);
 
